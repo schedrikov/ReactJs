df1b801eca207dbc379522fd36ab6192832836d1
schedrikov <schedrikov.denis@gmail.com>
2019-11-10 01:54:22 +0400

    Create project

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navigation() {
+    return (
+        <nav className="main-navigation">
+            <span><a href="/">Home</a></span>
+            <span><a href="/info/">Info</a></span>
+            <span><a href="/about/">About</a></span>
+        </nav>
+    );
+}
+
+export default Navigation;
